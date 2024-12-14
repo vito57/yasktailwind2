cfb53b6ba368246402d0e0b15eb4072d83317f0d
vito57 <vito57rus@yandex.ru>
2024-12-14 20:40:38 +0300

    text balance on h1

--- app/components/client.tsx
+++ app/components/client.tsx
@@ -170,7 +170,7 @@ export function Jumbo({ datas }: JumboProps) {
   return (
     <h1
       key={pathname}
-      className="lg:text-4xl font-display whitespace-pre-wrap	 text-3xl mx-auto max-w-xl antialiased gradientMe text-transparent bg-clip-text bg-[size:700px_700px] bg-center animate-fadeIn"
+      className="lg:text-4xl font-display text-balance text-3xl mx-auto max-w-xl antialiased gradientMe text-transparent bg-clip-text bg-[size:700px_700px] bg-center animate-fadeIn"
     >
       {title.title}
     </h1>
